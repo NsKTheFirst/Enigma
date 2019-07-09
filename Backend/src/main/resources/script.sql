10bc96746dad851d4ad9fe6cd363a144ea88344e
LOISEL Dominick <39958651+Dolois@users.noreply.github.com>
2019-07-09 15:43:39 +0200

    Create script.sql

---
+++ Backend/src/main/resources/script.sql
@@ -0,0 +1,61 @@
+USE 'enigma';
+
+INSERT INTO 'page' (nom, imageUrl, text, left, right, back, clue, position)
+VALUES (
+'Porte',
+' https://imgur.com/juwrHV3',
+'J’y suis… Je ne réalise toujours pas que l’on me donne accès à la tristement célèbre maison d’Amityville pour un article.',
+'',
+'',
+'',
+'1',
+?);
+
+INSERT INTO 'page' (nom, imageUrl, text, left, right, back, clue, position)
+VALUES (
+'Hall',
+'https://imgur.com/48qourp',
+'J’y suis… Je ne réalise toujours pas que l’on me donne accès à la tristement célèbre maison d’Amityville pour un article',
+'',
+'',
+'',
+'1',
+?);
+
+INSERT INTO 'page' (nom, imageUrl, text, left, right, back,clue, position)
+VALUES (
+'Cuisine',
+' https://imgur.com/wXUH39E',
+'La cuisine est plutôt propre...',
+'2',
+'4',
+'',
+'',
+?);
+
+INSERT INTO 'page' (nom, imageUrl, text, left, right, back, clue, position)
+VALUES (
+'Salon',
+'https://imgur.com/qGF7Iuu',
+'Le salon a été décoré avec goût.',
+'3',
+'5',
+'',
+'',
+?);
+
+INSERT INTO 'page' (nom, imageUrl, text, left, right, back, clue, position)
+VALUES (
+'Porte',
+'https://imgur.com/B9YEXdm',
+'La pièce qui m\'intéresse le plus est derrière cette porte… forcément fermée par un cadenas sur lequel on peut lire “µ&$”. Je peux surement trouver trouver la bonne combinaison.',
+'4',
+'2',
+'',
+'',
+?);
+
+INSERT INTO 'page' (nom, imageUrl, text, left, right, back, clue, position)
+VALUES (
+'',
+);
