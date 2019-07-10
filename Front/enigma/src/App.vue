b526214665e7f2e57584c1e6d8e3357d661cc973
zeboudj <olivia>
2019-07-10 17:40:59 +0200

    add of style and first call api user

--- Front/enigma/src/App.vue
+++ Front/enigma/src/App.vue
@@ -1,38 +1,30 @@
 <template>
   <div id="app">
-<!--    <div id="nav">-->
-<!--      <router-link to="/">Home</router-link> |-->
-<!--      <router-link to="/about">About</router-link>-->
-<!--    </div>-->
-    <Header/>
-    <router-view/>
+    <!--    <div id="nav">-->
+    <!--      <router-link to="/">Home</router-link> |-->
+    <!--      <router-link to="/about">About</router-link>-->
+    <!--    </div>-->
+    <router-view />
   </div>
 </template>
 
-<script>
-  import Header from '@/components/Header/Header.vue'
-  export default {
-    components: {
-      Header
-    }
-  }
-</script>
+
 
 <style lang="scss">
-    * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        appearance: scrollbarbutton-up;
-    }
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  appearance: scrollbarbutton-up;
+}
 
 #app {
+  background-color: #323232;
+  height: 100vh;
+}
 
-  /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
-  /*-webkit-font-smoothing: antialiased;*/
-  /*-moz-osx-font-smoothing: grayscale;*/
-  /*text-align: center;*/
-  /*color: #2c3e50;*/
+a {
+  text-decoration: none;
 }
 /*#nav {*/
 /*  padding: 30px;*/
--- Front/enigma/src/Services/usersService.js
+++ Front/enigma/src/Services/usersService.js
@@ -1,3 +1,44 @@
-export default {
+const axios = require("axios");
 
-}
+export default {
+  //create user
+  createUser(userData) {
+    axios
+      .post("http://localhost:8080/users", userData)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  },
+
+  //loge user
+  logUser(userPassword, userName) {
+    let details = {
+      username: userName,
+      password: userPassword
+    };
+
+    let formBody = [];
+    for (let property in details) {
+      let encodedKey = encodeURIComponent(property);
+      let encodedValue = encodeURIComponent(details[property]);
+      formBody.push(encodedKey + "=" + encodedValue);
+    }
+    formBody = formBody.join("&");
+    console.log("formBody ", JSON.stringify(formBody));
+    fetch("http://localhost:8080/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body: formBody,
+      mode: "no-cors",
+      withCredentials: true,
+      credentials: "include"
+    }).then(responseData => {
+      console.log(JSON.stringify(responseData));
+    });
+  }
+};
