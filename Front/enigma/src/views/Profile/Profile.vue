9dfd0b6b7eb3b08ee2578096c68d4bf6fbb86726
Fadi NOUFAL <noufal4me@gmail.com>
2019-07-11 21:46:17 +0200

    Front exemple

--- Front/enigma/src/views/Profile/Profile.vue
+++ Front/enigma/src/views/Profile/Profile.vue
@@ -2,12 +2,19 @@
 <style scoped src="./profile.css"></style>
 
 <script>
-    import Header from '@/components/Header/Header.vue'
-    export default {
-        name: "Profile",
-        components: {
-            Header
-        },
-    }
-</script>
+import usersServices from "@/Services/usersService";
 
+import Header from "@/components/Header/Header.vue";
+export default {
+  name: "Profile",
+  components: {
+    Header
+  },
+  methods: {
+    getUser() {
+      event.preventDefault();
+      usersServices.getProfile();
+    }
+  }
+};
+</script>
