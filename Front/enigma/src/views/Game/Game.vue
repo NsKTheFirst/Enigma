69d8598e1821c2b91f60246ba48647fd860201dd
NsKTheFirst <nicolas_jouenne@live.fr>
2019-07-11 23:49:45 +0200

    page jeu terminée, manque timer

--- Front/enigma/src/views/Game/Game.vue
+++ Front/enigma/src/views/Game/Game.vue
@@ -11,17 +11,78 @@
         },
         data(){
             return {
-                page: {}
+                page: {},
+                pointedPage: null,
+                currentPage: 1,
+                porte: false,
+                cases: [],
+                code: null,
+                retry: false
             }
         },
         mounted(){
-            this.getPage()
+            this.getFirstPage()
+            this.createCases()
         },
         methods:{
-            async getPage(){
+            async getFirstPage(){
                 let result = await pagesService.fetchPage(1)
                 console.log('page', result.data)
                 this.page = result.data
+            },
+            async getPage(cell){
+                // !this.page.back_ar ? this.currentPage = this.page.numPage : this.currentPage = this.page.back_ar
+                // console.log("currentPage", this.currentPage)
+                if(cell === 11 && cell ===this.page.position){
+                    this.pointedPage = 2
+                }
+                if(cell === 16 && this.currentPage === 2){
+                    this.pointedPage = 6
+                }
+                if(cell === 10 && cell ===this.page.position){
+                    this.pointedPage = 7
+                }
+                if(cell === 16 && this.currentPage === 4){
+                    this.pointedPage = 8
+                }
+                if(this.currentPage < 5 && cell !== 17 && cell === this.page.position){
+                    let result = await pagesService.fetchPage(this.pointedPage)
+                    console.log('page', result.data)
+                    this.page = result.data
+                    !this.page.back_ar ? this.currentPage = this.page.numPage : this.currentPage = this.page.back_ar
+                    console.log("currentPage", this.currentPage)
+                }
+                if(cell === 17){
+                    this.porte = true
+                }
+            },
+            async left(){
+                console.log("left")
+                let result = await pagesService.fetchPage(this.page.leftAr)
+                this.page = result.data
+                this.currentPage = this.page.numPage
+            },
+            async right(){
+                console.log("right")
+                let result = await pagesService.fetchPage(this.page.rightAr)
+                this.page = result.data
+                this.currentPage = this.page.numPage
+            },
+            async back(){
+                console.log("back")
+                let result = await pagesService.fetchPage(this.page.backAr)
+                this.page = result.data
+                this.currentPage = this.page.numPage
+            },
+            createCases(){
+                for(let i = 1; i <= 28; i ++){
+                    this.cases.push(i)
+                }
+                console.log("cases", this.cases)
+            },
+            checkCode(){
+                console.log("code", this.code)
+                Number(this.code) === 536 ? this.$router.push({name: 'Bravo'}) : this.retry = true
             }
         }
     }
