1c95d672ec1ded1c242475a2fbcf450319f478ab
Fadi NOUFAL <noufal4me@gmail.com>
2019-07-11 21:43:03 +0200

    Add basic authentification

--- Backend/src/main/java/com/simplon/enigma/config/WebSecurityConfig.java
+++ Backend/src/main/java/com/simplon/enigma/config/WebSecurityConfig.java
@@ -5,8 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
@@ -14,6 +16,7 @@ import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuc
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -39,19 +42,32 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests()
+        http.cors().and()
+                // starts authorizing configurations
+                .authorizeRequests()
+                // ignoring the guest's urls "
                 .antMatchers("/login", "/security/authError",
                         "/security/login", "/security/logout", "/users",
                         "/users/dashboard","/users/saveScore","/users/page",
-                        "/users/userProfile","/users/saveScores")
-                .permitAll().and().formLogin().loginPage("/security/login")
-                .loginProcessingUrl("/login")
-                .successHandler(new AuthentificationLoginSuccessHandler())
-                .failureUrl("/security/authError").and().logout()
-                .invalidateHttpSession(true)
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/security/logout").and()
-                .authorizeRequests().anyRequest().authenticated();
+                        "/users/userProfile","/users/saveScores",
+                        "/account/register","/account/login","/logout",
+                        "/users/login")
+                .permitAll()
+                // authenticate all remaining URLS
+                .anyRequest().fullyAuthenticated().and()
+                /* "/logout" will log the user out by invalidating the HTTP Session,
+                 * cleaning up any {link rememberMe()} authentication that was configured, */
+                .logout()
+                .permitAll()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "POST"))
+                .and()
+                // enabling the basic authentication
+                .httpBasic().and()
+                // configuring the session on the server
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED).and()
+                // disabling the CSRF - Cross Site Request Forgery
+                .csrf().disable();
+        http.csrf().disable();
     }
 
     @Bean
@@ -92,5 +108,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         AppAuthProvider provider = new AppAuthProvider();
         provider.setUserDetailsService(userDetailsService);
         return provider;
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:8081");
+
+            }
+        };
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
     }
 }
