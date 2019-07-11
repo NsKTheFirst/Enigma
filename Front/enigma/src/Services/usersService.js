d747554a8cafc521043c4a04c89047f6e2aad030
Fadi NOUFAL <noufal4me@gmail.com>
2019-07-11 21:46:17 +0200

    Front exemple

--- Front/enigma/src/Services/usersService.js
+++ Front/enigma/src/Services/usersService.js
@@ -2,22 +2,47 @@ const axios = require("axios");
 
 export default {
   //create user
-  createUser(userData) {
-   return axios.post("http://localhost:8080/users", userData)
-      // .then(response => {
-      //   console.log(response);
-      // })
-      // .catch(error => {
-      //   console.log(error);
-      // });
+   createUser(userData) {
+    return axios
+      .post("http://localhost:8080/users", userData)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   },
 
   //loge user
-  async logUser(userPassword, userName) {
+  logUser(userPassword, userName) {
+
     let details = {
       username: userName,
       password: userPassword
     };
+
+    // creating base64 encoded String from user name and password
+    let base64Credential = btoa( userName+ ':' + userPassword);
+
+  console.log(base64Credential);
+  
+
+    return axios.get("http://localhost:8080/security/login" ,   {
+      headers: {
+      'Authorization': 'Basic '+base64Credential,
+      'Accept': 'application/json'
+    }
+  })
+    .map((response) => {
+    // login successful if there's a jwt token in the response
+    let user = response.json().principal;// the returned user object is a principal object
+    if (user) {
+      // store user details  in local storage to keep user logged in between page refreshes
+      localStorage.setItem('currentUser', JSON.stringify(user));
+    }
+  });
+
+
 
     let formBody = [];
     for (let property in details) {
@@ -27,15 +52,27 @@ export default {
     }
     formBody = formBody.join("&");
     console.log("formBody ", JSON.stringify(formBody));
-      fetch("http://localhost:8080/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded"
-        },
-        body: formBody,
-        mode: "no-cors",
-        withCredentials: true,
-        credentials: "include"
-      })
-    }
+    axios
+    .get("http://localhost:8080/users/login", {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json;charset=UTF-8',
+        'Access-Control-Allow-Origin': '*'
+      }
+    }).then(responseData => {
+      console.log(responseData);
+    });
+  },
+
+  getProfile() {
+    return axios
+      .get("http://localhost:8080/users/userProfile?name=fadi")
+      .then(response => {
+        console.log(response);
+      });
+  }
+
+
+
+
 };
