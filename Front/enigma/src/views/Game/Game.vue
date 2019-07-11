03a1316c1785a52448d3f1d2aca3809b3562aba1
NsKTheFirst <nicolas_jouenne@live.fr>
2019-07-11 15:14:18 +0200

    debut entrée datas en dur

--- Front/enigma/src/views/Game/Game.vue
+++ Front/enigma/src/views/Game/Game.vue
@@ -12,7 +12,18 @@
             return {
                 page: {
                     imageUrl: "https://zupimages.net/up/19/28/s5mb.png"
-                }
+                },
+                page1: {
+                    id: 1,
+                    nom: "Porte",
+                    imageUrl : "https://imgur.com/juwrHV3",
+                    text :  "J’y suis… Je ne réalise toujours pas que l’on me donne accès à la tristement célèbre maison d’Amityville pour un article.",
+                    left : null,
+                    right : null,
+                    back : null,
+                    clue : null,
+                    position : 11
+                },
             }
         }
     }
