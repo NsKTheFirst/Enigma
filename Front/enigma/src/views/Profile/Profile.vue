197cd3aa33984f3c7cab83babde00ca2e8f92ade
Dolois <dloisel@sfr.fr>
2019-07-11 11:51:12 +0200

    Creation et Stylisation de la page Profil

--- Front/enigma/src/views/Profile/Profile.vue
+++ Front/enigma/src/views/Profile/Profile.vue
@@ -2,8 +2,26 @@
 <style scoped src="./profile.css"></style>
 
 <script>
+    import Header from '@/components/Header/Header.vue'
     export default {
-        name: "Profile"
+        name: "Profile",
+        components: {
+            Header
+        },
+        data() {
+            return {
+                oldPassword: null,
+                newPassword: null
+            }
+        },
+        methods: {
+            updatePassword() {
+                console.log("Update cela marche")
+            },
+            delAccount() {
+                console.log("Youpi c'est supprimé")
+            }
+        }
     }
 </script>
 
