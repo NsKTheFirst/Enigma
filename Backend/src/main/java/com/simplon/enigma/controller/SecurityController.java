30ea6b00e11ebdd6b53aefc8ab88ffa5526a7cd8
Fadi NOUFAL <noufal4me@gmail.com>
2019-07-13 22:35:23 +0200

    Update security

--- Backend/src/main/java/com/simplon/enigma/controller/SecurityController.java
+++ Backend/src/main/java/com/simplon/enigma/controller/SecurityController.java
@@ -3,7 +3,6 @@ package com.simplon.enigma.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
